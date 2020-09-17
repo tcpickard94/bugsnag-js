e15c0049fdfdfa18b8480753a77c4059d60c3efa
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-09-17 21:39:33 +0100

    Debugging

--- test/expo/scripts/build-ios.sh
+++ test/expo/scripts/build-ios.sh
@@ -1,13 +1,22 @@
 #!
 
-set -e
 
 # Lets make sure the build folder was cleared out correctly
+echo rm
 rm -rf $BUILDKITE_BUILD_CHECKOUT_PATH/build/*
+echo cd
 cd test/expo/features/fixtures/test-app
+echo "npm i turtle"
 npm i turtle-cli@0.14 bunyan
+echo Perl
 perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/\@expo/xdl/build/detach/IosNSBundle.js
 perl -0777 -i.original -pe "s/entitlements\\['aps-environment'\\] =[^;]+;//gs" node_modules/turtle-cli/node_modules/\@expo/xdl/build/detach/IosNSBundle.js
+echo Team Id=$APPLE_TEAM_ID 
+echo EXPO_P12_PATH=$EXPO_P12_PATH
+echo EXPO_PROVISIONING_PROFILE_PATH=$EXPO_PROVISIONING_PROFILE_PATH
+echo EXPO_RELEASE_CHANNEL=$EXPO_RELEASE_CHANNEL
+echo BUILDKITE_BUILD_CHECKOUT_PATH=$BUILDKITE_BUILD_CHECKOUT_PATH
+
 node_modules/.bin/turtle build:ios \
   -c ./app.json \
   --team-id $APPLE_TEAM_ID \
